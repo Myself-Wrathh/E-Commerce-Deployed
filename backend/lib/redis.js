48a1a3be93235eda7bbafc824ea48b33a518ea8e
Karan <mrkaran2k5@gmail.com>
2025-01-04 22:02:19 +0530

    Redis Fix Due to inactivity

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,4 +1,4 @@
 import Redis from "ioredis"
 
-export const redis = new Redis("rediss://default:AXJKAAIjcDE3YTE5NzJkNzI4NmI0MGQ0OGEyODZhNmFkOTA3ZGY2NXAxMA@notable-cod-29258.upstash.io:6379");
+export const redis = new Redis("rediss://default:AVidAAIjcDE3MGU5NjUwMGZjODU0OTY4OWNlOTZhMGU1ZThiYmU1MnAxMA@poetic-firefly-22685.upstash.io:6379");
 await redis.set('foo2', 'bar2');
